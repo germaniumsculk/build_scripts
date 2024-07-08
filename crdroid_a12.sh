008095315059125a68a2ab30cdaf4ab851d742ca
musfiqurrahmanfahim <rmusfiqur971@gmail.com>
2024-07-08 12:21:23 +0600

    Create crdroid_a12.sh
    Just started yet...

---
+++ crdroid_a12.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/crdroidandroid/android.git -b 12.1 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/musfiqurrahmanfahim/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=F3
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh 
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_Mi439_4_19-userdebug
+echo "============="
+
+# Build rom
+mka build
