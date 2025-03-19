5738a52b05858400336d0c3eaad2029e235e991a
Germanium Sculk <germaniumsculk@gmail.com>
2025-03-19 11:16:40 +0600

    Create axion_a15.sh

---
+++ axion_a15.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/germaniumsculk/local_manifests_clo .repo/local_manifests -b axion-15-qpr2
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Curl Signing Files
+git clone https://github.com/germaniumsculk/certs
+
+#SIGNING_KEY_PATH ?= certs
+#RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
+#SIGNING_KEY_PATH ?= certs
+#RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
+#PRODUCT_DEFAULT_DEV_CERTIFICATE := $(RELEASE_KEY)
+#PRODUCT_OTA_PUBLIC_KEYS := $(RELEASE_KEY)
+
+# Have to add these flags in device.mk
+
+# Export
+export BUILD_USERNAME=Germanium 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+axion mi439 va
+
+# Build
+brunch mi439
