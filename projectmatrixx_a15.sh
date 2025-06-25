a63a77f18aa94b40b38ddd03b18d39f34ade407d
Germanium Sculk <germaniumsculk@gmail.com>
2025-06-25 23:28:22 +0600

    Create projectmatrixx_a15.sh

---
+++ projectmatrixx_a15.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/germaniumsculk/local_manifests_lavender .repo/local_manifests -b matrixx-vic-qpr2
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Curl Signing Files
+git clone https://github.com/germaniumsculk/certs
+
+#SIGNING_KEY_PATH ?= certs
+#RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
+#SIGNING_KEY_PATH ?= certs
+#RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
+#PRODUCT_DEFAULT_DEV_CERTIFICATE := $(RELEASE_KEY)
+#PRODUCT_OTA_PUBLIC_KEYS := $(RELEASE_KEY)
+
+# Have to add these flags in device.mk
+
+# FSgen removal
+rm -rf build/soong/fsgen
+
+# Export
+export BUILD_USERNAME=Germanium 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+lunch lineage_lavender-bp1a-userdebug
+
+# Build
+mka bacon
