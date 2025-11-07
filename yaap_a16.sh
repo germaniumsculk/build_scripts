e23796507be31435654389c2b8dc4de83dad6a3e
Germanium Sculk <germaniumsculk@gmail.com>
2025-11-07 08:52:54 +0600

    Update yaap_a16.sh

--- yaap_a16.sh
+++ yaap_a16.sh
@@ -5,9 +5,6 @@
 # --------------------------------------
 
 rm -rf .repo/local_manifests/
-rm -rf vendor
-rm -rf packages
-rm -rf external
 
 # repo init rom
 repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
