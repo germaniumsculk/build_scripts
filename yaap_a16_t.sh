0a54148a71a3ca61a01dbbbde2f5f3d1cb0520eb
Germanium Sculk <germaniumsculk@gmail.com>
2025-11-07 11:28:44 +0600

    Create yaap_a16_t.sh

---
+++ yaap_a16_t.sh
@@ -0,0 +1,15 @@
+# Export
+export BUILD_USERNAME=Germanium 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+lunch yaap_lavender-user
+
+# Build
+m yaap
